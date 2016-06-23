388e2f1a48de0308bbe75203bd0bd929f0c57c8d
Alma Jensen <alma.jensen@landesk.com>
2016-06-23 15:00:18 -0600

    Starting to implement DataServices
    Initial addition of data service includes a call to get the conference
    schedule.

---
+++ src/UtahOpenSource.DataStorage/API/APIClient.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using UtahOpenSource.DataObjects.UTOSDataModels;
+
+namespace UtahOpenSource.DataStorage.API
+{
+    public class APIClient
+    {
+        public async Task<IEnumerable<Talk>> GetAllSessions()
+        {
+            var resultStream = await GetHttpResultStream(Resources.SessionsURL);
+            if (resultStream != null)
+            {
+                using (var reader = new JsonTextReader(new StreamReader(resultStream)))
+                    return JsonSerializer.Create().Deserialize<IEnumerable<Talk>>(reader);
+            }
+            return null;
+        }
+        protected async Task<Stream> GetHttpResultStream(string url)
+        {
+            HttpClient client = new HttpClient();
+            var result = await client.GetAsync(url);
+            if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                return await result.Content.ReadAsStreamAsync();
+            else
+                return null;
+        }
+    }
+}
---
+++ src/UtahOpenSource.DataStorage/API/Resources.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtahOpenSource.DataStorage.API
+{
+    public class Resources
+    {
+        public const string SessionsURL = "http://www.openwest.org/custom/api.php?f=session_list&amp;h=1";
+        public const string SponsorsURL = "http://www.openwest.org/custom/api.php?f=sponsor_list&amp;h=1";
+        public const string MapURL = "http://www.openwest.org/custom/api.php?f=map_list";
+    }
+}
